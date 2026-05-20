fd8cac1030fc3d1918dd6619d0f346627a16ab95
altrup <51763643+altrup@users.noreply.github.com>
2026-05-19 20:41:52 -0400

    fix custom execs

--- dots/.config/hypr/custom/execs.lua
+++ dots/.config/hypr/custom/execs.lua
@@ -1,6 +1,6 @@
 -- Input method
 hl.on("hyprland.start", function()
-    hl.dsp.exec_cmd("fcitx5")
-    hl.dsp.exec_cmd("hyprpm reload")
-    hl.dsp.exec_cmd("nm-applet --indicator")
+    hl.exec_cmd("fcitx5")
+    hl.exec_cmd("hyprpm reload")
+    hl.exec_cmd("nm-applet --indicator")
 end)
